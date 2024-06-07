d3525c1b8b0ed4848259e56c0510e1fff7e32876
Hai Nguyen <quochai.kstn@gmail.com>
2024-06-08 00:37:44 +0700

    ruff and pre-commit hook

--- lint.py
+++ lint.py
@@ -14,9 +14,10 @@ if __name__ == "__main__":
     for i in range(0, 1000):
         logging.info(i)
         publisher.publish_add_number_celery(1, 2)
-        #publisher.publish_add_number_tenacity(1, 2)
+        # publisher.publish_add_number_tenacity(1, 2)
         time.sleep(2)
-     
-     
-def dirty():  
-    logging.info("dirty code")   
+
+
+def dirty():
+    # app.ok()  # to make pre-commit fail
+    logging.info("dirty code")
